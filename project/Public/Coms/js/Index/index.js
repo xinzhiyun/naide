0c82af41baaaee312b9405b7507059d141fc8eee
zero9527 <zero8297183@163.com>
2018-04-25 10:57:37 +0800

    修改页面跳转

--- project/Public/Coms/js/Index/index.js
+++ project/Public/Coms/js/Index/index.js
@@ -3,7 +3,7 @@ new Vue({
     el: ".main",
     data: {
         // 用户信息 userName姓名 balance余额 canMoney可提现
-        userInfo: {userName : "嗯嗯", balance: split("33567", ".", 3) , canMoney: split("23534", ".", 3)},
+        userInfo: {userName : "嗯嗯", balance: splitStr("33567", ".", 3) , canMoney: splitStr("23534", ".", 3)},
         // 经销商收益
         dealer: {},
         // 服务站收益
