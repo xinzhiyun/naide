b5d6dbca34b95d4e0fe0493a5da74738c73df586
XUELINYU <570600268@qq.com>
2018-04-26 10:30:32 +0800

    修改服务记录点击跳转功能

--- project/Public/Home/js/Users/Service_Log/service_log.js
+++ project/Public/Home/js/Users/Service_Log/service_log.js
@@ -4,7 +4,7 @@ var service_log_vue = new Vue({
 		// 安装
 		install:[
 			{
-				order_number:"2018040225652",//工单号
+				order_number:"2018040225601",//工单号
 				status:"进行中",//状态
 				binding_time:"2018-03-31",//绑定的时间
 				install_personnel:"嗯呢",//安装人员
@@ -18,7 +18,7 @@ var service_log_vue = new Vue({
 		// 维修
 		repair:[
 			{
-				order_number:"2018040225652",
+				order_number:"2018040225602",
 				status:"已完成",
 				binding_time:"2018-03-31",
 				install_personnel:"嗯呢",//安装人员
@@ -28,7 +28,7 @@ var service_log_vue = new Vue({
 				service_date:"2018-03-31",//安装时间
 			},
 			{
-				order_number:"2018040225652",
+				order_number:"201804022503",
 				status:"进行中",
 				binding_time:"2018-03-31",
 				install_personnel:"嗯呢",//安装人员
@@ -41,7 +41,7 @@ var service_log_vue = new Vue({
 		// 维护
 		maintenance:[
 			{
-				order_number:"2018040225652",
+				order_number:"2018040225604",
 				status:"已完成",
 				binding_time:"2018-03-31",
 				install_personnel:"哈哈",//安装人员
@@ -51,7 +51,7 @@ var service_log_vue = new Vue({
 				service_date:"2018-03-31",//安装时间
 			},
 			{
-				order_number:"2018040225652",
+				order_number:"2018040225605",
 				status:"进行中",
 				binding_time:"2018-03-31",
 				install_personnel:"咔咔",//安装人员
@@ -61,7 +61,7 @@ var service_log_vue = new Vue({
 				service_date:"2018-03-31",//安装时间
 			},
 			{
-				order_number:"2018040225652",
+				order_number:"2018040225606",
 				status:"已完成",
 				binding_time:"2018-03-31",
 				install_personnel:"卡吗",//安装人员
@@ -73,16 +73,35 @@ var service_log_vue = new Vue({
 		],
 		// 服务详情页面
 		service_details_info:{},
+		code:"",
 		num: [1, 2, 3],//安装，维修，维护信息数量
 	},
 	methods:{
 		// 服务详情页面，点击将选中的“整条信息”以实参的方式传入，赋值给“service_details_bg”服务详情页面
-		service_details_page:function(info){
+		service_details_page:function(info,number){
+			location.href = location.href+"?index=1"+"&"+number;
 			var that = this;
 			$(".install_user").hide();
 			$("#service_details_bg").show();
-			// console.log(info);
-			that.service_details_info = info;
+			that.service_details_info =  info;
+
+			// localStorage
+			var info_all = JSON.stringify(info);
+			if(!window.localStorage){
+				alert("浏览器支持localStorage！");
+			}else{
+				var storage = window.localStorage;
+				for(var i = 0;i<localStorage.length;i++){
+					// 库中所有的键
+			        var storage_value_all = localStorage.key(i);
+			        var number_all = storage_value_all.split("_")[2];
+			        if(number_all == number){
+			        	that.service_details_info = JSON.parse(storage.getItem(localStorage.key(i)));
+			        }
+				}
+				storage.setItem("order_number_"+number,info_all);
+				// that.service_details_info =  JSON.parse(storage.getItem("order_number_"+number));
+			}
 		},
 		showModule:function(event){
 			var el = event.currentTarget || event.srcElement;
@@ -129,4 +148,4 @@ var service_log_vue = new Vue({
 
 });
 // $(".install_user").hide();
-$("#service_details_bg").hide();
+// $("#service_details_bg").hide();
