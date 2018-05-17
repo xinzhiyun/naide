1e8ecbfb6e5ac9e370daeac24e21b32138a08e39
李振东 <lzdong@foxmail.com>
2018-05-17 10:03:32 +0800

    雷振东的修改—系统设置

--- project/Application/Admin/Controller/SystemController.class.php
+++ project/Application/Admin/Controller/SystemController.class.php
@@ -12,20 +12,44 @@ use Org\Util\Strings;
 class SystemController extends CommonController
 {
 	/**
-     * 系统设置-收款设置
+     * 系统设置
      */
     public function index()
     {
-
+        $info = M('system')->find(1);
+        $this->assign('info',$info);
+        $this->display();
     }
 
-    /**
-     * 系统设置-水机购买协议
-     */
-    public function agreement()
+    public function edit()
     {
 
+        try {
+            $data = I('post.');
+            if (empty($data['key'])) {
+                E('数据错误', 201);
+            }
+
+            $save[$data['key']] = trim($data['val'],'%');
+            $save['updatetime'] = time();
+            $res = M('system')->where('id=1')->save($save);
+
+            if($res){
+               E('修改成功', 200);
+            }
+            E('修改失败', 201);
+        } catch (\Exception $e) {
+            $this->to_json($e);
+        }
     }
+
+//    /**
+//     * 系统设置-水机购买协议
+//     */
+//    public function agreement()
+//    {
+//
+//    }
 
 
 }
