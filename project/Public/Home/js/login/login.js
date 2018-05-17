0ae2e97a00d490a7240955e38ce833507f23af25
xiaojuan <2536663785@qq.com>
2018-05-17 18:43:59 +0800

    忘记密码

--- project/Public/Home/js/login/login.js
+++ project/Public/Home/js/login/login.js
@@ -1,6 +1,7 @@
 $(function() {
+    var codeFlag = false;//存放验证码
     // 表单提交
-    $("#forms").on("submit", function() {
+    $(".login").on("click", function() {
         // 验证
         // 手机号码
         if($("input[name='username']").val()) {
@@ -15,7 +16,10 @@ $(function() {
         // 验证码
         if($("input[name='code']").val()) {
             // 判断得到的验证码跟用户输入的验证码是否一致
-
+            if(!codeFlag) {
+                noticeFn({text: "验证码出错!"});
+                return false; 
+            }
         }else {
             noticeFn({text: "验证码不能为空!"});
             return false;
@@ -41,26 +45,70 @@ $(function() {
             noticeFn({text: "确认密码不能为空!"});
             return false;
         }
+        /*
+            user	账号
+            password	密码
+            re_password	重复密码
+            code	验证码
+        */
+        var url = getURL("Home", "login/reg");
+        // 提交表单
+        $.ajax({
+            url: url,
+            type: "post",
+            data: {
+                user: $("input[name='username']").val(),
+                password: $("input[name='newpass']").val(),
+                re_password: $("input[name='confirmpass']").val(),
+                code: $("input[name='code']").val()
+            },
+            success: function(res) {
+                console.log("提交成功", res);
+                if(res.status == 200) {
+                    noticeFn({text: "提交成功"});
+                    window.location.href = "{{:U('Login/index.html')}}";
+                }else {
+                    noticeFn({text: "系统出错，请稍后再试!"});
+                }
+            },
+            error: function(res) {
+                console.log("失败", res); 
+                noticeFn({text: "系统出错，请稍后再试!"});
+            }
+        })
     })
-    // 点击获取验证码
-    var codeVal = "";//存放验证码
+    // 点击提交验证码
     $(".getCode").on("click", function() {
         // 手机号码
-        if($("input[name='username']").val()) {
-            if(!phoneCheck($("input[name='username']").val())) {
+        var phone = $("input[name='username']").val();
+        if(phone) {
+            if(!phoneCheck(phone)) {
                 noticeFn({text: "手机号码格式错误!"});
                 return false;
             }else {
+                // md5加密手机号码取8位，从2截取。
+                var md5 = hex_md5(phone.substr(2, 8));
+                var url = getURL("Home", "login/send");
                 // 获取验证码
                 $.ajax({
-                    url: "",
+                    url: url,
                     type: "post",
-                    data: $("input[name='username']").val(),
+                    data: {
+                        phone:phone,
+                        tocken: md5
+                    },
                     success: function(res) {
-                        
+                        console.log("成功", res);
+                        if(res.status == 200) {
+                            codeFlag = true;
+                        }else {
+                            codeFlag = false;
+                        }
                     },
                     error: function(res) {
-                        
+                       console.log("失败", res); 
+                       codeFlag = false;
+                       noticeFn({text: "系统出错，请稍后再试!"});
                     }
                 })
             }
