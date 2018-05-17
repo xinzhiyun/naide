540afe8ae7fdd4373855a735db39aba51c2bafca
李振东 <lzdong@foxmail.com>
2018-05-17 11:16:58 +0800

    雷振东的修改—经销商费用审核

--- project/Application/Admin/Controller/SystemController.class.php
+++ project/Application/Admin/Controller/SystemController.class.php
@@ -50,6 +50,70 @@ class SystemController extends CommonController
 //    {
 //
 //    }
+    /**
+     * 经销商费用设置审核
+     */
+    public function setdealer()
+    {
+        $list = M('vendors')->where(['examine=1'])->select();
+        $arr = [
+            'install_price'=>['price'],
+            'service_price'=>['price'],
+            'commission'=>['price'],
+            'install_price_ex'=>['price'],
+            'service_price_ex'=>['price'],
+            'commission_ex'=>['price'],
+        ];
+        $list = replace_array_value($list,$arr);
+        $this->assign('list',$list);
+        $this->display();
+    }
 
+    /**
+     * 审核
+     */
+    public function setdealeraction()
+    {
+        try {
+            $data = I('post.');
+            if (empty($data['id'])) {
+                E('数据错误', 201);
+            }
+            if (!isset($data['rel'])) {
+                E('数据错误', 201);
+            }
+            $saveData['examine']=0;
+            $saveData['commission_ex']=0;
+            $saveData['service_price_ex']=0;
+            $saveData['install_price_ex']=0;
+
+            $info = M('vendors')->find($data['id']);
+
+            if ($data['rel']==1) {
+                $saveData['commission']    = $info['commission_ex'];
+                $saveData['service_price'] = $info['service_price_ex'];
+                $saveData['install_price'] = $info['install_price_ex'];
+
+                $res = M('vendors')->where('id='.$data['id'])->save($saveData);
+
+            } else {
+
+                $res = M('vendors')->where('id='.$data['id'])->save($saveData);
+            }
+
+
+
+
+
+            if($res){
+                E('修改成功', 200);
+            }
+            E('修改失败', 201);
+        } catch (\Exception $e) {
+            $this->to_json($e);
+        }
+    }
+    
+    
 
 }
