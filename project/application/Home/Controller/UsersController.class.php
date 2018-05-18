a9b814884e379b641dbf583d4dba2550c2d8ddea
李振东 <lzdong@foxmail.com>
2018-05-18 15:26:55 +0800

    雷振东的修改—迁移邀请码

--- project/application/Home/Controller/UsersController.class.php
+++ project/application/Home/Controller/UsersController.class.php
@@ -122,32 +122,32 @@ class UsersController extends HomebaseController {
         $this->assign('info',$info);
         $this->display();
     }
-    //每个用户的邀请码
-    function create_guid($namespace = '') {
-        static $guid = '';
-        $uid = uniqid("", true);
-        $data = $namespace;
-        $data .= $_SERVER['REQUEST_TIME'];
-        $data .= $_SERVER['HTTP_USER_AGENT'];
-        $data .= $_SERVER['LOCAL_ADDR'];
-        $data .= $_SERVER['LOCAL_PORT'];
-        $data .= $_SERVER['REMOTE_ADDR'];
-        $data .= $_SERVER['REMOTE_PORT'];
-        $hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
-        $guid = substr($hash,  0,  8) .
-            '-' .
-            substr($hash,  8,  4) .
-            '-' .
-            substr($hash, 12,  4) .
-            '-' .
-            substr($hash, 16,  4) .
-            '-' .
-            substr($hash, 20, 12) ;
-
-
-        return $guid;
-
-    }
+//    //每个用户的邀请码 迁移到pay
+//    function create_guid($namespace = '') {
+//        static $guid = '';
+//        $uid = uniqid("", true);
+//        $data = $namespace;
+//        $data .= $_SERVER['REQUEST_TIME'];
+//        $data .= $_SERVER['HTTP_USER_AGENT'];
+//        $data .= $_SERVER['LOCAL_ADDR'];
+//        $data .= $_SERVER['LOCAL_PORT'];
+//        $data .= $_SERVER['REMOTE_ADDR'];
+//        $data .= $_SERVER['REMOTE_PORT'];
+//        $hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
+//        $guid = substr($hash,  0,  8) .
+//            '-' .
+//            substr($hash,  8,  4) .
+//            '-' .
+//            substr($hash, 12,  4) .
+//            '-' .
+//            substr($hash, 16,  4) .
+//            '-' .
+//            substr($hash, 20, 12) ;
+//
+//
+//        return $guid;
+//
+//    }
 
     /**
      * 建议
