3ce09361c9e4d2f3f972fae563481d59808844da
袁湖海 <meikoul@163.com>
2018-05-21 17:48:11 +0800

    方法换到对应的控制器

--- project/application/Coms/Controller/ServiceController.class.php
+++ project/application/Coms/Controller/ServiceController.class.php
@@ -4,6 +4,20 @@ use Common\Controller\ComsbaseController;
 
 class ServiceController extends ComsbaseController {
 
+    //服务记录列表
+    public function index() {
+        $map['vid'] = session('comsuser.id');
+        $per_list = M('work')->field('status,no,addtime')->where($map)->select();
+        if ($per_list) {
+            $tone = M('work')->where(['vid'=>$map['vid'],'type'=>0])->count();
+            $ttwo= M('work')->where(['vid'=>$map['vid'],'type'=>1])->count();
+            $tf= M('work')->where(['vid'=>$map['vid'],'type'=>2])->count();
+
+            $this->ajaxReturn(['code'=>200,'data'=>$per_list,'tone'=>$tone,'ttwo'=>$ttwo,'tf'=>$tf]);
+        } else {
+            $this->ajaxReturn(['code'=>400]);
+        }
+    }
 }
 
 
