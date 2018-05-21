4a9c3c19b3bd30a597708135b65ef4874a90b9a9
袁湖海 <meikoul@163.com>
2018-05-21 11:51:49 +0800

    修改变量

--- project/application/Coms/Controller/UsersController.class.php
+++ project/application/Coms/Controller/UsersController.class.php
@@ -247,9 +247,9 @@ class UsersController extends ComsbaseController {
         $ttwo= M('work')->where(['vid'=>$id,'type'=>1])->count();
         $tf= M('work')->where(['vid'=>$id,'type'=>2])->count();
 //        $this->ajaxReturn(['code'=>200,'tone'=>$tone,'ttwo'=>$ttwo,'tf'=>$tf]);
-        $this->assign('tone',$tone);
-        $this->assign('ttwo',$ttwo);
-        $this->assign('tf',$tf);
+        $this->assign('tone',json_encode($tone));
+        $this->assign('ttwo',json_encode($ttwo));
+        $this->assign('tf',json_encode($tf));
     }
     //待办任务列表
     public function sevice_list() {
