05c58408608f1d616f6042037b1199fda04ef3ac
XUELINYU <570600268@qq.com>
2018-05-22 11:15:28 +0800

    获取服务记录数据

--- project/Public/Coms/js/service_log/service_log.js
+++ project/Public/Coms/js/service_log/service_log.js
@@ -1,80 +1,19 @@
 var service_log_vue = new Vue({
 	el:"#service_log_vue",
 	data:{
+		info:{},
 		// 安装
-		install:[
-			{
-				order_number:"2018040225601",//工单号
-				status:"进行中",//状态
-				binding_time:"2018-03-31",//绑定的时间
-				install_personnel:"嗯呢",//安装人员
-				phone_number:"13526342511",//手机号码
-				service_type:"安装",//安装类型
-				service_content:"安装到什么，安装到哪里",//安装内容
-				service_date:"2018-03-31",//安装时间
-
-			}
-		],
+		install:[],
 		// 维修
-		repair:[
-			{
-				order_number:"2018040225602",
-				status:"已完成",
-				binding_time:"2018-03-31",
-				install_personnel:"嗯呢",//安装人员
-				phone_number:"13526342511",//手机号码
-				service_type:"维修",//安装类型
-				service_content:"灯不亮",//安装内容
-				service_date:"2018-03-31",//安装时间
-			},
-			{
-				order_number:"201804022503",
-				status:"进行中",
-				binding_time:"2018-03-31",
-				install_personnel:"嗯呢",//安装人员
-				phone_number:"13526342511",//手机号码
-				service_type:"维修",//安装类型
-				service_content:"开关不亮",//安装内容
-				service_date:"2018-03-31",//安装时间
-			},
-		],
+		repair:[],
 		// 维护
-		maintenance:[
-			{
-				order_number:"2018040225604",
-				status:"已完成",
-				binding_time:"2018-03-31",
-				install_personnel:"哈哈",//安装人员
-				phone_number:"13526342511",//手机号码
-				service_type:"维护",//安装类型
-				service_content:"换芯-1级",//安装内容
-				service_date:"2018-03-31",//安装时间
-			},
-			{
-				order_number:"2018040225605",
-				status:"进行中",
-				binding_time:"2018-03-31",
-				install_personnel:"咔咔",//安装人员
-				phone_number:"13526342511",//手机号码
-				service_type:"维护",//安装类型
-				service_content:"换芯-2级",//安装内容
-				service_date:"2018-03-31",//安装时间
-			},
-			{
-				order_number:"2018040225606",
-				status:"已完成",
-				binding_time:"2018-03-31",
-				install_personnel:"卡吗",//安装人员
-				phone_number:"13526342511",//手机号码
-				service_type:"维护",//安装类型
-				service_content:"换芯-3级",//安装内容
-				service_date:"2018-03-31",//安装时间
-			},
-		],
+		maintenance:[],
 		// 服务详情页面
 		service_details_info:{},
 		code:"",
-		num: [1, 2, 3],//安装，维修，维护信息数量
+		num: [],//安装，维修，维护信息数量
+		getAjax:"",
+
 	},
 	methods:{
 		// 跳转页面改变url（公共）
@@ -90,28 +29,35 @@ var service_log_vue = new Vue({
 			}
 		},
 		// 服务详情页面，点击将选中的“整条信息”以实参的方式传入，赋值给“service_details_bg”服务详情页面
-		service_details_page:function(info,number){
+		service_details_page:function(status,number){
+			// service_log_vue.url("1",number);
+			var _this = this;
+			// $(".install_user").hide();
+			// $("#service_details_bg").show();
+			// _this.service_details_info =  info;
+			// // 将获取数据存入localStorage，避免页面刷新后数据为空
+			// var info_all = JSON.stringify(info);
+			// if(!window.localStorage){
+			// 	alert("浏览器支持localStorage！");
+			// }else{
+			// 	var storage = window.localStorage;
+			// 	for(var i = 0;i<localStorage.length;i++){
+			// 		// 库中所有的键
+			//         var storage_value_all = localStorage.key(i);
+			//         var number_all = storage_value_all.split("_")[2];
+			//         if(number_all == number){
+			//         	_this.service_details_info = JSON.parse(storage.getItem(localStorage.key(i)));
+			//         }
+			// 	}
+			// 	storage.setItem("order_number_"+number,info_all);
+			// }
+			console.log(number)
+			var url = getURL("Coms","users/sevice_details.html?index=1&no="+number);
+			var data = {index:status,no:number};			
+			_this.getAjax(url,data);
 			service_log_vue.url("1",number);
-			var that = this;
 			$(".install_user").hide();
-			$("#service_details_bg").show();
-			that.service_details_info =  info;
-			// 将获取数据存入localStorage，避免页面刷新后数据为空
-			var info_all = JSON.stringify(info);
-			if(!window.localStorage){
-				alert("浏览器支持localStorage！");
-			}else{
-				var storage = window.localStorage;
-				for(var i = 0;i<localStorage.length;i++){
-					// 库中所有的键
-			        var storage_value_all = localStorage.key(i);
-			        var number_all = storage_value_all.split("_")[2];
-			        if(number_all == number){
-			        	that.service_details_info = JSON.parse(storage.getItem(localStorage.key(i)));
-			        }
-				}
-				storage.setItem("order_number_"+number,info_all);
-			}
+			$("#service_details_bg").show(1000)
 		},
 		showModule:function(event){
 			var el = event.currentTarget || event.srcElement;
@@ -149,13 +95,46 @@ var service_log_vue = new Vue({
 			for(var i = 0;i<li_span.length;i++){
 				if(li_span[i].innerHTML == "进行中"){
 					li_span[i].setAttribute("style","color:#f00");
-				}else{
+				}else if(li_span[i].innerHTML == "已完成"){
 					li_span[i].setAttribute("style","color:#4AD7BB");
+				}else if(li_span[i].innerHTML == "未处理"){
+					li_span[i].setAttribute("style","color:#000");
 				}
 			}
 		});
+	},
+	mounted:function(){
+		var _this = this;
+		_this.getAjax = function(url,data){
+			$.ajax({
+				url:url,
+				type:"post",
+				data:data,
+				success:function(res){
+					if(res.code == 200){
+						console.log("成功",res)
+						// if(res.data != ""){
+							sessionStorage.setItem("service_details_info",JSON.stringify(res.data));
+							console.log(res.data)
+							// _this.service_details_info=res.data;
+						// }
+						// callback({res:res.data,msg:0});
+					}else{
+						// callback({res:"",text:"系统出错，请稍候再试！",msg:1});
+					}
+				},
+				error:function(res){
+					// callback({res:"",text:"系统出错，请稍候再试！",msg:1});
+				}
+			})
+		};
+		// 服务记录详情
+		var detaile = JSON.parse(sessionStorage.getItem("service_details_info"));
+		if(detaile){
+			_this.service_details_info = detaile;
+			console.log(_this.service_details_info);
+		}
 	}
-
 });
 // $(".install_user").hide();
-// $("#service_details_bg").hide();
+// $("#service_details_bg").show();	      
--- project/application/Coms/Controller/ServiceController.class.php
+++ project/application/Coms/Controller/ServiceController.class.php
@@ -6,20 +6,21 @@ class ServiceController extends ComsbaseController {
 
     //服务记录列表
     public function index() {
+
         $map['vid'] = session('comsuser.id');
         $per_list = M('work')->field('status,no,addtime')->where($map)->select();
-
+        
         if ($per_list) {
             $tone = M('work')->where(['vid' => $map['vid'], 'type' => 0])->count();
             $ttwo = M('work')->where(['vid' => $map['vid'], 'type' => 1])->count();
             $tf = M('work')->where(['vid' => $map['vid'], 'type' => 2])->count();
-            $this->assign('data', $per_list);
+            $this->assign('data', json_encode($per_list));
             $this->assign('tone', $tone);
             $this->assign('ttwo', $ttwo);
             $this->assign('tf', $tf);
-            $this->display();
-
         }
+
+         $this->display();
     }
 }
 
