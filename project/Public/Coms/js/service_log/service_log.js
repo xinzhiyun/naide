660401b2faf5cfc91cd6f43e20aa41fd03f910d8
XUELINYU <570600268@qq.com>
2018-05-22 13:31:22 +0800

    修改服务记录页面跳转

--- project/Public/Coms/js/service_log/service_log.js
+++ project/Public/Coms/js/service_log/service_log.js
@@ -17,78 +17,54 @@ var service_log_vue = new Vue({
 	},
 	methods:{
 		// 跳转页面改变url（公共）
-		url:function(num,number){
+		url:function(status,number){
 			var url = window.document.location.href.toString();
 			var href = url.split("?")[0];
 			var href2 = url.split("&")[1];
 			// 判断第一次点击有没有刷新，
 			if(href2 == undefined){
-				location.href = location.href + "?index="+ num + "&" + number;
+				location.href = location.href + "?index="+ status + "&no=" + number;
 			}else{
-				history.replaceState({}, null, href +"?index="+ num + "&" + number);
+				history.replaceState({}, null, href +"?index="+ status + "&no=" + number);
 			}
 		},
 		// 服务详情页面，点击将选中的“整条信息”以实参的方式传入，赋值给“service_details_bg”服务详情页面
-		service_details_page:function(status,number){
-			// service_log_vue.url("1",number);
-			var _this = this;
-			// $(".install_user").hide();
-			// $("#service_details_bg").show();
-			// _this.service_details_info =  info;
-			// // 将获取数据存入localStorage，避免页面刷新后数据为空
-			// var info_all = JSON.stringify(info);
-			// if(!window.localStorage){
-			// 	alert("浏览器支持localStorage！");
-			// }else{
-			// 	var storage = window.localStorage;
-			// 	for(var i = 0;i<localStorage.length;i++){
-			// 		// 库中所有的键
-			//         var storage_value_all = localStorage.key(i);
-			//         var number_all = storage_value_all.split("_")[2];
-			//         if(number_all == number){
-			//         	_this.service_details_info = JSON.parse(storage.getItem(localStorage.key(i)));
-			//         }
-			// 	}
-			// 	storage.setItem("order_number_"+number,info_all);
-			// }
-			console.log(number)
-			var url = getURL("Coms","users/sevice_details.html?index=1&no="+number);
-			var data = {index:status,no:number};			
-			_this.getAjax(url,data);
-			service_log_vue.url("1",number);
-			$(".install_user").hide();
-			$("#service_details_bg").show(1000)
+		sevice_details:function(status,number){
+			service_log_vue.url(status,number);
 		},
-		showModule:function(event){
-			var el = event.currentTarget || event.srcElement;
-			var $el = $(el);
-			var attr_name = $el.attr("class");
-			switch(attr_name){
-				case "all_page":
-					$(".install_ul").show().siblings().show();
-					$(".install_user").show();
-					$("#service_details_bg").hide();
-					break;
-				case "install":
-					$(".install_ul").show().siblings().hide();
-					$(".install_user").show();
-					$("#service_details_bg").hide();
-					break;
-				case "repair":
-					$(".repair_ul").show().siblings().hide();
-					$(".install_user").show();
-					$("#service_details_bg").hide();
-					break;
-				case "maintenance":
-					$(".maintenance_ul").show().siblings().hide();
-					$(".install_user").show();
-					$("#service_details_bg").hide();
-					break;
-			}
-		},
+		// showModule:function(event){
+		//  @click='showModule($event)'
+		// 	var el = event.currentTarget || event.srcElement;
+		// 	var $el = $(el);
+		// 	var attr_name = $el.attr("class");
+		// 	console.log(attr_name)
+		// 	switch(attr_name){
+		// 		case "all_page":
+		// 			$(".install_ul").show().siblings().show();
+		// 			$(".install_user").show();
+		// 			$("#service_details_bg").hide();
+		// 			break;
+		// 		case "install":
+		// 			$(".install_ul").show().siblings().hide();
+		// 			$(".install_user").show();
+		// 			$("#service_details_bg").hide();
+		// 			break;
+		// 		case "repair":
+		// 			$(".repair_ul").show().siblings().hide();
+		// 			$(".install_user").show();
+		// 			$("#service_details_bg").hide();
+		// 			break;
+		// 		case "maintenance":
+		// 			$(".maintenance_ul").show().siblings().hide();
+		// 			$(".install_user").show();
+		// 			$("#service_details_bg").hide();
+		// 			break;
+		// 	}
+		// },sevice_details
 	},
 	// 状态(字体样式)
 	created:function(){
+		var _this = this;
 		$(function(){
 			var a = $(".status").html();
 			var li_span = $(".install_user_content>ul>li .status");
@@ -102,23 +78,24 @@ var service_log_vue = new Vue({
 				}
 			}
 		});
-	},
-	mounted:function(){
-		var _this = this;
-		_this.getAjax = function(url,data){
+		var url = window.document.location.href.toString();
+		var href = url.split("=")[1];
+		if(href){
+			var index = url.split("=")[1].split("&")[0];
+			var no = url.split("=")[2];
+			// var url = HTTP
+			var url = getURL("Coms","users/sevice_details.html?index="+index+"&no="+no);
+			console.log(url)
 			$.ajax({
 				url:url,
-				type:"post",
-				data:data,
+				type:"get",
+				// data:data,
 				success:function(res){
 					if(res.code == 200){
 						console.log("成功",res)
-						// if(res.data != ""){
-							sessionStorage.setItem("service_details_info",JSON.stringify(res.data));
-							console.log(res.data)
-							// _this.service_details_info=res.data;
-						// }
-						// callback({res:res.data,msg:0});
+							_this.service_details_info=res.data;
+							$(".install_user").hide();
+							$("#service_details_bg").show();
 					}else{
 						// callback({res:"",text:"系统出错，请稍候再试！",msg:1});
 					}
@@ -127,12 +104,7 @@ var service_log_vue = new Vue({
 					// callback({res:"",text:"系统出错，请稍候再试！",msg:1});
 				}
 			})
-		};
-		// 服务记录详情
-		var detaile = JSON.parse(sessionStorage.getItem("service_details_info"));
-		if(detaile){
-			_this.service_details_info = detaile;
-			console.log(_this.service_details_info);
+
 		}
 	}
 });
