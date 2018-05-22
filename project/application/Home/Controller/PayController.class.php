a601466b5a20b9f6e0fa6832ce8d76164fd6f236
袁湖海 <meikoul@163.com>
2018-05-22 14:21:27 +0800

    bug

--- project/application/Home/Controller/PayController.class.php
+++ project/application/Home/Controller/PayController.class.php
@@ -441,7 +441,7 @@ class PayController extends AppframeController {
                 E('该手机号已存在',201);
             }
             if (empty($info)) {
-                if (empty(session('waterOrder.code'))) {
+                if (empty($data['to_code'])) {
                     E('邀请码不能为空', 201);
                 } else {
                     $users_code =  M('users')->field('code,to_code')->where(['code'=>session('waterOrder.code')])->find();
