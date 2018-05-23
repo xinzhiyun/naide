4cb9d711acadf75883b6e58ab0712fa1658238d2
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-05-23 22:27:00 +0800

    梁康伦的修改—提现修改

--- project/application/Home/Controller/WithdrawController.class.php
+++ project/application/Home/Controller/WithdrawController.class.php
@@ -11,12 +11,35 @@ class WithdrawController extends HomebaseController
 	public function index()
 	{
 		if (IS_POST) {
+			//接收提现金额
+			$data['money'];
 			
+			//开户名
+			$data['name'];
+
+			//银行名
+			$data['choose'];
+			
+			//开户支行
+			$data['acc_open'];
+			
+			//银行卡号
+			$data['bank'];
+
+			//用户id
+			$data['uid'] = $_SESSION['homeuser']['id'];
+
+			//创建时间
+			$data['create_time'] = time();
+
+
 		} else {
 			//查询登录用户的的余额
 			$balance = M('users')->field('balance')->where('id='.$_SESSION['homeuser']['id'])->find()['balance'];
 
-
+			//分配可提现余额
+			$this->assign('enmoney', $balance);
+			$this->display();
 
 			//查询用户是否使用过的银行账户
 			$banks = M('bank')->where('uid='.$_SESSION['homeuser']['id'])->select();
@@ -28,9 +51,7 @@ class WithdrawController extends HomebaseController
 				
 			}
 
-			//分配可提现余额
-			$this->assign('enmoney', $balance);
-			$this->display();
+			
 		}
 		
 	}
