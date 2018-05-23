62177ee093b3fbacef45d98afe3339cd39810c73
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-05-23 22:16:57 +0800

    梁康伦的修改—提现修改

--- project/application/Home/Controller/WithdrawController.class.php
+++ project/application/Home/Controller/WithdrawController.class.php
@@ -10,34 +10,31 @@ class WithdrawController extends HomebaseController
 	 */
 	public function index()
 	{
-		//查询登录用户的的余额
-		$balance = M('users')->field('balance')->where('id='.$_SESSION['homeuser']['id'])->find()['balance'];
-
-
-
-		//查询用户是否使用过的银行账户
-		$banks = M('bank')->where('uid='.$_SESSION['homeuser']['id'])->select();
-
-		if (empty($banks)) {
-			$this->ajaxReturn(array('code'=>'400','msg'=>'没有账户'));
-		} else {
-			$this->ajaxReturn(array('code'=>'200','msg'=>$banks));
+		if (IS_POST) {
 			
+		} else {
+			//查询登录用户的的余额
+			$balance = M('users')->field('balance')->where('id='.$_SESSION['homeuser']['id'])->find()['balance'];
+
+
+
+			//查询用户是否使用过的银行账户
+			$banks = M('bank')->where('uid='.$_SESSION['homeuser']['id'])->select();
+
+			if (empty($banks)) {
+				$this->ajaxReturn(array('code'=>'400','msg'=>'没有账户'));
+			} else {
+				$this->ajaxReturn(array('code'=>'200','msg'=>$banks));
+				
+			}
+
+			//分配可提现余额
+			$this->assign('enmoney', $balance);
+			$this->display();
 		}
-
-		//分配可提现余额
-		$this->assign('enmoney', $balance);
-		$this->display();
+		
 	}
 
-	/**
-	 * [getMessage 获取提现信息]
-	 * @return [type] [description]
-	 */
-	public function getMessage()
-	{
-
-	}
 }
 
 
