ba898c8b8773158e096640b1c685032a9266a695
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-05-24 10:20:24 +0800

    梁康伦的修改—前端提现完成

--- project/application/Home/Controller/WithdrawController.class.php
+++ project/application/Home/Controller/WithdrawController.class.php
@@ -13,7 +13,7 @@ class WithdrawController extends HomebaseController
 		//查询登录用户的的余额
 		$balance = M('users')->field('balance,canmoney')->where('id='.$_SESSION['homeuser']['id'])->find();
 
-		dump($balance);
+		// dump($balance);
 		//分配可提现余额
 		$this->assign('enmoney', $balance);
 		$this->display();
@@ -51,10 +51,21 @@ class WithdrawController extends HomebaseController
 		//可提现余额
 		$canBalance = $balance['balance']-$balance['canmoney'];
 
+		if ($canBalance < 0) {
+			$this->ajaxReturn(array('code'=>'400','msg'=>'可取余额为零'));
+		}
+
+		//如果提现金额大于可提余额，则取最大可取余额
 		if ($_POST['data']['money'] > $canBalance) {
-			$data['money'] = $users['canmoney'] = $canBalance;
+			$data['money'] = $canBalance;
+
+			//冻结余额=之前的冻结余额+本次提现金额
+			$users['canmoney'] = $balance['canmoney'] + $_POST['data']['money'];
 		} else {
-			$data['money'] = $users['canmoney'] = $_POST['data']['money'];
+			$data['money'] =  $_POST['data']['money'];
+
+			//冻结余额=之前的冻结余额+本次提现金额
+			$users['canmoney'] = $balance['canmoney'] + $_POST['data']['money'];
 		}
 		
 		//开户名
@@ -75,13 +86,20 @@ class WithdrawController extends HomebaseController
 		//创建时间
 		$data['create_time'] = time();
 
-		$info = M('Bank')->add($data);
+		//开启事物
+		M('Bank')->startTrans();
+		M('Users')->startTrans();
 
-		$bool = M('Users')->where('id='.$_SESSION['homeuser']['id'])->save($users['canmoney']);
+		$info = M('Bank')->add($data);
+		$bool = M('Users')->where('id='.$_SESSION['homeuser']['id'])->save($users);
 
 		if ($info && $bool) {
+			M('Bank')->commit();
+			M('Users')->commit();
 			$this->ajaxReturn(array('code'=>'200','msg'=>'提交成功'));	
 		} else {
+			M('Bank')->rollback();
+			M('Users')->rollback();
 			$this->ajaxReturn(array('code'=>'400','msg'=>'提交失败'));
 		}
 	}
