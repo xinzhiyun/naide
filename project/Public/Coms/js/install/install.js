5a9466f3da28e781ab9a7cf7b22ad2b1076e7f74
xiaojuan <2536663785@qq.com>
2018-05-24 18:06:06 +0800

    安装人员搜索

--- project/Public/Coms/js/install/install.js
+++ project/Public/Coms/js/install/install.js
@@ -2,41 +2,77 @@
 var home = new Vue({
     el: ".main",
     data: {
-        // 用户列表
-        userList: {},
-        search:"",
+        userList: [
+            {
+                name: "",
+                phone: "暂无数据",
+                create_time: ""
+            }
+        ],// 用户列表
+        search:"",//存放搜索值
     },
     methods: {
         // 点击搜索小图标提交表单
         subClick() {
-            this.sub_pub()
+            var _this = this;
+            $(".loadingdiv").fadeIn('slow');
+            _this.sub_pub(trimFn(_this.search), function(res) {
+                // 显示搜索出来的安装人员列表
+                _this.userList = res.list;
+                $(".loadingdiv").fadeOut('fast');
+            });
+        },
+        // 回车搜索
+        searchs: function() {
+            $(".loadingdiv").fadeIn('slow');
+            var _this = this;
+            _this.sub_pub(trimFn(_this.search), function(res) {
+                // 显示搜索出来的安装人员列表
+                _this.userList = res.list;
+                $(".loadingdiv").fadeOut('fast');
+            });
+            console.log(234)
         },
         // 搜索(公共部分)
-        sub_pub:function(){
-            var _this = this;
-            var sub = [];
-            if(chineseCheck(trimFn(this.search)) || phoneCheck(this.search)){
-                var detail = home.userList;
-                for(var i = 0;i<detail.length;i++){                 
-                    if(_this.search == detail[i].name || detail[i].name == detail[i].phone){
-                        sub.push(detail[i])
+        sub_pub: function(searchVal, callback){
+            var url = getURL("Coms", "Install/search");
+            $.ajax({
+                url: url,
+                type: "post",
+                data: {search: searchVal}, 
+                success: function(res) {
+                    console.log("搜索成功", res);
+                    if(res.status == 200) {
+                        if(res.list.length) {
+                            $(".icon-xiangyou1").show();
+                            callback(res);
+                        }else {
+                            $(".icon-xiangyou1").hide();
+                            home.userList = [{
+                                name: "",
+                                phone: "暂无数据",
+                                create_time: ""
+                            }];
+                            $(".loadingdiv").fadeOut('fast');
+                        }
+                    }else {
+
                     }
-                }          
-                if(sub.length != 0){
-                    _this.sevice_list = [];
-                    _this.sevice_list = sub;
-                    $(".userLi").show();
-                }else{
-                    _this.service_details_info = detail;
-                    noticeFn({text:'没有搜索到匹配的信息!'});
-                    $(".userLi").hide();
+                },
+                error: function(res) {
+                    console.log("失败", res);
                 }
-            }
+            })
         },
         installedit:function(id){
+            var _this = this;
+            // 判断用户信息是否存在
+            if(_this.userList[0].phone == "暂无数据") {
+                return false;
+            }
             var url = getURL('Coms', 'install/installEdit');
             location.href = url + "?id=" + id;
-
+            
         }
     },
 });
