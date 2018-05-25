6198f237e09ecf0307811c7dd47212f5781447f1
李振东 <lzdong@foxmail.com>
2018-05-25 14:39:05 +0800

    雷振东的修改—用户注册时间

--- project/application/Home/Controller/UsersController.class.php
+++ project/application/Home/Controller/UsersController.class.php
@@ -60,7 +60,7 @@ class UsersController extends HomebaseController {
             $info = $m->where('user='.$reg['user'])->find();
 
             if (empty($info)) {
-                $data['created_at']=time();
+                $reg['created_at']=time();
                 $reg['code'] = $this->create_guid();
                 //老父亲
                 $reg['to_code'] = $users_code['code'];
